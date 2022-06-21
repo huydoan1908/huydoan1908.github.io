88e168aeabfe2881c7f7c8f48bfb0175bc540829
Huy Doan <huy11900@gmail.com>
2022-06-21 16:16:24 +0700

    Revert "refactor"
    This reverts commit a541a719d7e0696aea7e0c8bf3398d32a6ccd862.

---
+++ weather-app/src/App.js
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { ThemeProvider, Backdrop, CircularProgress } from "@mui/material";
+import { StoreContext } from "./store";
+// Import Component
+import SearchBar from "./components/SearchBar";
+import Main from "./components/Main";
+//Import Style
+import "./App.css";
+import darkTheme from "./themes/darkTheme";
+
+function App() {
+  const [state, dispatch] = useContext(StoreContext);
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <SearchBar />
+        <Main />
+        <Backdrop open={state.backdrop}>
+          <CircularProgress color="info" />
+        </Backdrop>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
